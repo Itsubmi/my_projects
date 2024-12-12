93c7817500cf57d3bb9a0ad4ca7ca3a060750b4c
Tseren-Ubushieva Izhilina <itsubmiloopse@gmail.com>
2024-12-12 16:48:12 +0300

    Delete funcItem.h

--- funcItem.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include "Creatures.h"
-#include "Items.h"
-void printItem(Items item);
-Items createWeapon(const char name[30], int damage);
-Items createArmor(const char name[30], int defense);
-Items createPotion(const char name[30], int healingAmount);
