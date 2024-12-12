3464a9c3c8f1748f72d80dde5898e8906ee874d8
Tseren-Ubushieva Izhilina <itsubmiloopse@gmail.com>
2024-12-12 16:48:18 +0300

    Delete you.bat

--- you.bat
+++
@@ -1,6 +0,0 @@
-gcc FuncChest.c -c -o chest.obj
-gcc FuncLocation.c -c -o loc.obj
-gcc FuncCreatures.c -c -o creature.obj
-gcc main.c -c -o main.obj
-gcc main.obj creature.obj loc.obj chest.obj -o my_program.exe
-my_program.exe
